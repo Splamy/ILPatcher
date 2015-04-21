1e37433139ffb44a65cf5eebc84f8a4ae279b192
Splamy <splamyn@gmail.com>
2015-04-21 12:31:01 +0200

    Idea with abstract PatchActionForm

---
+++ ILPatcher/PatchAction/EditorPatchAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ILPatcher
+{
+	abstract class EditorPatchAction : Control
+	{
+		public abstract void SetPatchAction(PatchAction pa);
+
+		protected Action<PatchAction> ParentAddCallback;
+	}
+}
